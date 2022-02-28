8411dc4fcc802721ca25f00f432700a35214d651
Biplob123121 <biplobkb123@gmail.com>
2022-03-01 04:11:40 +0600

    single phone details completed

--- js/mobile-mart.js
+++ js/mobile-mart.js
@@ -29,7 +29,7 @@ const displayPhone = phones =>{
                  <div class="card-body">
                  <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">Brand: ${phone.brand}</p>
-                <button onclick="loadPhoneDetails(${phone.slug})" class="details-btn">Details</button>
+                <button onclick="loadPhoneDetails('${phone.slug}')" class="details-btn">Details</button>
                 </div>
             </div>
         `
@@ -37,6 +37,33 @@ const displayPhone = phones =>{
     }
 }
 
+//  single phone details load
+
 const loadPhoneDetails = phoneId =>{
-    console.log(phoneId.slug);
+    //console.log(phoneId);
+    fetch(`https://openapi.programming-hero.com/api/phone/${phoneId}`)
+    .then(res => res.json())
+    .then(data => displayPhoneDetails(data.data));
+}
+
+const displayPhoneDetails = phone =>{
+    console.log(phone);
+    const phoneDetailsContainer = document.getElementById('phone-details');
+    phoneDetailsContainer.innerHTML = '';
+    const div = document.createElement('div');
+    div.innerHTML =`
+        <div class="card">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+             <div class="card-body">
+                <h5 class="card-title">Release Date: ${phone.releaseDate}</h5>
+                <h5 class="card-title">Memory: ${phone.mainFeatures.memory}</h5>
+                <h5 class="card-title">Chip Set: ${phone.mainFeatures.chipSet}</h5>
+                <h5 class="card-title">WLAN: ${phone.others.WLAN}</h5>
+                <h5 class="card-title">Bluetooth: ${phone.others.Bluetooth}</h5>
+                <h5 class="card-title">GPS: ${phone.others.GPS}</h5>
+                 <a href="#" class="btn btn-primary">Go somewhere</a>
+             </div>
+        </div>
+    `
+    phoneDetailsContainer.appendChild(div);
 }
