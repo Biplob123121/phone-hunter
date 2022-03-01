3692ad04bb02e275044202bc2c8049c776e9412e
Biplob123121 <biplobkb123@gmail.com>
2022-03-01 15:36:20 +0600

    spinner added

--- js/mobile-mart.js
+++ js/mobile-mart.js
@@ -14,7 +14,8 @@ const loadPhones = () =>{
     }
 }
 
-const displayPhone = phones =>{
+const displayPhone = allPhones =>{
+    const phones = allPhones.slice(0, 20);
     //console.log(phones);
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.innerHTML = '';
@@ -31,7 +32,7 @@ const displayPhone = phones =>{
         div.classList.add('col');
         div.innerHTML = `
              <div class="card h-100">
-                 <img src="${phone.image}" class="card-img-top" alt="...">
+             <img src="${phone.image}" class="card-img-top" alt="...">
                  <div class="card-body">
                  <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">Brand: ${phone.brand}</p>
@@ -50,6 +51,8 @@ const loadPhoneDetails = phoneId =>{
     fetch(`https://openapi.programming-hero.com/api/phone/${phoneId}`)
     .then(res => res.json())
     .then(data => displayPhoneDetails(data.data));
+    //.catch(error => console.log(error));
+    
 }
 
 const displayPhoneDetails = phone =>{
@@ -57,20 +60,22 @@ const displayPhoneDetails = phone =>{
     const phoneDetailsContainer = document.getElementById('phone-details');
     phoneDetailsContainer.innerHTML = '';
     const div = document.createElement('div');
+
     div.innerHTML =`
         <div class="card">
             <img src="${phone.image}" class="card-img-top" alt="...">
              <div class="card-body">
-                <h5 class="card-title">Release Date: ${phone.releaseDate}</h5>
+                <h5 class="card-title">Release Date: ${phone.releaseDate ? phone.releaseDate : 'Unknown Release Date'}</h5>
                 <h5 class="card-title">Memory: ${phone.mainFeatures.memory}</h5>
                 <h5 class="card-title">Chip Set: ${phone.mainFeatures.chipSet}</h5>
                 <h5 class="card-title">Sensors: ${phone.mainFeatures.sensors}</h5>
-                <h5 class="card-title">WLAN: ${phone.others.WLAN}</h5>
-                <h5 class="card-title">Bluetooth: ${phone.others.Bluetooth}</h5>
-                <h5 class="card-title">GPS: ${phone.others.GPS}</h5>
+                <h5 class="card-title">WLAN: ${phone.others?.WLAN ? phone.others.WLAN : 'None'}</h5>
+                <h5 class="card-title">Bluetooth: ${phone.others?.Bluetooth? phone.others.Bluetooth : 'None'}</h5>
+                <h5 class="card-title">GPS: ${phone.others?.GPS? phone.others.GPS : 'None'}</h5>
                  <a href="#" class="btn btn-primary">Go somewhere</a>
              </div>
         </div>
     `
+    //phoneDetailsContainer.appendChild(h5);
     phoneDetailsContainer.appendChild(div);
 }
